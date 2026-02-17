e2044820a6e5cf9220f92ce8aa6a34abba83facc
Ashita  D Jawali <113736983+Ashita2619@users.noreply.github.com>
2026-02-17 13:16:56 -0600

    Update pre_mycosnp.py
    Added the script

--- scripts/pre_mycosnp/pre_mycosnp.py
+++ scripts/pre_mycosnp/pre_mycosnp.py
@@ -1,1 +1,33 @@
+import subprocess
+import sys
+import os
+
+def run_pre_mycosnp(run_dir):
+    # Extract run_date from run_dir
+    run_date = os.path.basename(run_dir.rstrip("/"))
+    
+    samplesheet = os.path.join(run_dir, "samplesheet.csv")
+    
+    output_dir = f"/epi/home/ashita.jawali@kdhe.state.ks.us/WGS_Drive/Cauris/Output/Pre_mycosnp/{run_date}"
+
+    if not os.path.exists(samplesheet):
+        raise FileNotFoundError(f"SampleSheet not found: {samplesheet}")
+
+    cmd = (
+        f"cd {run_dir} && "
+        "source $HOME/.bashrc && "
+        "source /epi/home/ashita.jawali@kdhe.state.ks.us/mambaforge/etc/profile.d/conda.sh && "
+        "conda activate nextflow && "
+        "nextflow run CDCgov/mycosnp-nf "
+        "--workflow PRE_MYCOSNP "
+        "-profile docker "
+        f"--input {samplesheet} "
+        #"--add_sra_file srr.csv "
+        f"--outdir {output_dir} "
+    )
+
+    subprocess.run(cmd, shell=True, executable="/bin/bash")
+    
+
+
 
