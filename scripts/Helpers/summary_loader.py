922a13d80566ca4fcd87eac66197be2938c57fdc
Ashita  D Jawali <113736983+Ashita2619@users.noreply.github.com>
2026-06-11 12:59:33 -0500

    Update summary_loader.py
    Updated script for creating assembly metrics

--- scripts/Helpers/summary_loader.py
+++ scripts/Helpers/summary_loader.py
@@ -1,1 +1,60 @@
+import pandas as pd
 
+
+def load_summary_data(summary_csv):
+
+    df = pd.read_csv(summary_csv)
+
+    sample_HSN = df["Sample"].tolist()
+
+    clade_data = {}
+
+    assembly_metrics = {}
+
+    for _, row in df.iterrows():
+
+        hsn = str(row["Sample"])
+
+        #################################################
+        # CLADE
+        #################################################
+
+        clade = str(
+            row.get("Subtype_Closest_Match", "")
+        ).split("-")[0]
+
+        clade_data[hsn] = {
+
+            "clade": clade
+        }
+
+        #################################################
+        # METRICS
+        #################################################
+
+        assembly_metrics[hsn] = [{
+
+            "assembly_length": 
+                row.get(
+                    "Sample_Assembly_Length",
+                    ""
+                ),
+
+            "avg_depth_coverage":
+                row.get(
+                    "Avg_Depth_Coverage",
+                    ""
+                ),
+
+            "trimmed_reads":
+                row.get(
+                    "Trimmed_Reads",
+                    ""
+                )
+        }]
+
+    return (
+        sample_HSN,
+        clade_data,
+        assembly_metrics
+    )
